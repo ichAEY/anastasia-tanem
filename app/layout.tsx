1c4d4ce31421f02f185219afa7029e650ccb37ad
tanem <melikyan.mac@gmail.com>
2026-08-13 12:44:49 +0300

    Add Yandex Metrika counter

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,17 @@ export const metadata: Metadata = {
   other: { "codex-preview": "development" },
 };
 
+const yandexMetrikaCode = `
+(function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=111558185', 'ym');
+
+ym(111558185, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+`;
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
@@ -43,6 +54,19 @@ export default function RootLayout({
     <html lang="ru">
       <body>
         {children}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{ __html: yandexMetrikaCode }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/111558185"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
         <a
           id="yclients-booking-proxy"
           className="ms_booking yclients-booking-proxy"
